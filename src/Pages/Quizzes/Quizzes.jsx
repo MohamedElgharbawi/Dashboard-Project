8520e1cfe875c3f97519776b961d7c9944e20f76
MohamedElgharbawi <mohamedkhaledmohamed291@gail.com>
2025-06-13 07:14:31 +0300

    Edit

--- src/Pages/Quizzes/Quizzes.jsx
+++ src/Pages/Quizzes/Quizzes.jsx
@@ -1,4 +1,30 @@
+import { useAdmin } from "../../Components/Context/UserProvider";
+import axios from "axios";
+import { useEffect } from "react";
 const Quizzes = () => {
+    
+    const { token } = useAdmin();
+    async function getData() {
+        console.log(token);
+        try {
+            const options = {
+                url: "https://brightminds.runasp.net/api/Quizzes",
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            }
+            const data  = await axios.request(options);
+            console.log(data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    useEffect(() => {
+        if(token)
+            getData();
+    }, [token])
+    
     return (
         <>
             Quizzes
